86296b3504977fe3583431f5f9d4990cca4e95bd
Alexpattanaik <lxpattanaik@gmail.com>
2025-11-06 09:12:54 +0530

    function added for process and add it in db.

--- read data and process it.py
+++ read data and process it.py
@@ -1,8 +1,26 @@
 import pandas as pd
-def read_data ():
-    csv1 = '/Users/mac/Downloads/order_region_a(in).csv'
-    csv2 = '/Users/mac/Downloads/order_region_b(in).csv'
+import ast
+import json
+from sqlalchemy import create_engine
+def read_data (file1,file2):
+    csv1 = file1
+    csv2 = file2
 
     dfa = pd.read_csv(csv1)
     dfb = pd.read_csv(csv2)
     return (dfa,dfb)
+def process(dfa,dfb):
+    dfa["region"] = 'A'
+    dfb["region"] = 'B'
+    combo = pd.concat([dfa, dfb], ignore_index=True)
+    combo["total_sales"] = combo["QuantityOrdered"] * combo["ItemPrice"]
+    combo = combo.drop_duplicates(subset="OrderId", keep="first")
+    combo["PromotionDiscount"] = combo["PromotionDiscount"].apply(ast.literal_eval)
+    combo["net_sale"] = combo["total_sales"] - combo["PromotionDiscount"].apply(lambda x: float(x["Amount"]))
+    combo = combo[combo["net_sale"] > 0]
+    combo["PromotionDiscount"] = combo["PromotionDiscount"].apply(json.dumps)
+    combo.reset_index(drop=True, inplace=True)
+    return combo
+def add_data_to_sql(combo):
+    engine = create_engine("sqlite:///sales_data.db")
+    combo.to_sql("sales_table", engine, if_exists="replace", index=False)
